e78f4dc41598c0b972fba7ab35a7df2da884207a
ziehlke <ziehlke@gmail.com>
2019-01-15 19:49:12 +0100

    init

---
+++ src/main/java/net/ddns/ziehlke/eletopo/EletopoApplication.java
@@ -0,0 +1,14 @@
+package net.ddns.ziehlke.eletopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletopoApplication.class, args);
+	}
+
+}
+
