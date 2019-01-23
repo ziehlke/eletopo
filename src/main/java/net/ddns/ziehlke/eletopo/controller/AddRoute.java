42995a5fb0eff197b11e2e8ff6c9b3966b5fb363
ziehlke <ziehlke@gmail.com>
2019-01-23 20:39:25 +0100

    fix add post form, add route model to controller

--- src/main/java/net/ddns/ziehlke/eletopo/controller/AddRoute.java
+++ src/main/java/net/ddns/ziehlke/eletopo/controller/AddRoute.java
@@ -8,23 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/route")
 public class AddRoute {
     private final RouteService routeService;
 
     @GetMapping("/add")
     public String addRouteGet(@ModelAttribute("route") Route route, Model model) {
-        return "route";
+        model.addAttribute("route", route);
+        return "add";
     }
 
 
     @PostMapping
     public String addRoutePost(@ModelAttribute("route") Route route, Model model) {
         routeService.save(route);
-        return "route";
+        return "add";
     }
 }
