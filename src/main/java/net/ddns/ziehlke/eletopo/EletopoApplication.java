cc486f01d2a40bc2d70d90d56e02a34cb8cf6657
ziehlke <ziehlke@gmail.com>
2019-02-05 11:54:43 +0100

    merge with feature_security

--- src/main/java/net/ddns/ziehlke/eletopo/EletopoApplication.java
+++ src/main/java/net/ddns/ziehlke/eletopo/EletopoApplication.java
@@ -2,6 +2,8 @@ package net.ddns.ziehlke.eletopo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 public class EletopoApplication {
